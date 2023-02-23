7ebb0c5abdf25613a8375ae9bd3bbbca45b7d692
neusfer <125355473+neusfer@users.noreply.github.com>
2023-02-23 09:11:17 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Steps)
+    basic.pause(3000)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    Steps += 1
+})
+let Steps = 0
+let Set = 0
+basic.forever(function () {
+    if (input.acceleration(Dimension.Strength) > 500) {
+        Steps += 1
+        basic.showNumber(Steps)
+    }
+})
